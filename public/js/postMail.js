81d0990e8f497e84885b3e31bbd360944a05304e
pupoSDC <pupo163@hotmail.com>
2019-04-08 21:04:49 +0200

    Preparations for PHP server integration
    - Removed useless files
    - Moved all public files to public folder
    - Create Email interaction endpoint

---
+++ public/js/postMail.js
@@ -0,0 +1,20 @@
+(() => {
+	const mailForm = document.getElementById("contact-form");
+	const email = document.getElementById("form-email");
+	const submit = mailForm.querySelector(".submit");
+
+	submit.onclick = (event) => {
+		event.preventDefault();
+		email.value;
+		if (email.checkValidity()) {
+			fetch("subscribe", {
+	          method: 'post',
+	          body: JSON.stringify({ email })
+	        });
+		} else {
+			email.toggleAttribute("error", true);
+		}
+	}
+
+	email.onchange = () => email.toggleAttribute("error", false);
+})()
